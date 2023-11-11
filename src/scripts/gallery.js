50a74e7f194f2f2dee9bbadb7410f2516c2e5041
Gravydigger <gravydigger65@gmail.com>
2023-11-11 15:03:04 +1030

    Added Gallery page with image popup on click

---
+++ src/scripts/gallery.js
@@ -0,0 +1,46 @@
+const images = document.querySelectorAll(".image");
+const modal = document.getElementsByClassName("modal")[0];
+const close = document.getElementsByClassName("close")[0];
+const body = document.getElementsByTagName("body")[0];
+
+images.forEach((image) => {
+  image.addEventListener("click", (event) => {
+    displayModal(event);
+  });
+  image.addEventListener("keydown", (event) => {
+    // Ignore if key is tab
+    if (
+      event.isComposing ||
+      event.keyCode == 9 ||
+      modal.style.display == "block"
+    ) {
+      return;
+    }
+    displayModal(event);
+  });
+});
+
+function displayModal(event) {
+  var element = event.target;
+  var modalImg = document.getElementsByClassName("modal-content")[0];
+
+  if (element | modal | (modalImg == null)) return;
+
+  if (element.tagName == "LI") {
+    element = element.lastChild;
+  }
+
+  modal.style.display = "flex";
+  body.style.overflow = "hidden";
+
+  modalImg.src = element.attributes[0].nodeValue;
+}
+modal.onclick = function () {
+  modal.style.display = "none";
+  body.style.overflow = "auto";
+};
+
+close.onkeydown = function () {
+  modal.style.display = "none";
+  body.style.overflow = "auto";
+};
